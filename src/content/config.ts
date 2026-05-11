6ba30f3f581d4734ef4c4c5afe18f813c6b1277e
INDUC TECH <163207787+sauron897@users.noreply.github.com>
2026-05-10 20:41:13 -0400

    Modify blog schema in config.ts
    Updated author default value and added readingTime field to blog schema.

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,17 @@
 import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    pubDate: z.coerce.date(),
-    keywords: z.array(z.string()).optional(),
-    heroImage: z.string().optional(),
-    author: z.string().default('Editor'),
-    draft: z.boolean().default(false),
-  }),
+    type: 'content',
+    schema: z.object({
+          title: z.string(),
+          description: z.string(),
+          pubDate: z.coerce.date(),
+          keywords: z.array(z.string()).optional(),
+          heroImage: z.string().optional(),
+          author: z.string().default('INDUC TECH'),
+          readingTime: z.number().optional(),
+          draft: z.boolean().default(false),
+    }),
 });
 
 export const collections = { blog };
